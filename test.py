77dee09e0366392e0d5c3983b02a1725285096b1
mohamadlakkis <mohamad.allakkis@gmail.com>
2024-12-04 21:51:05 +0300

    Done Fine Tuning Q & A Model Stopped at setp 382 (since it stabilized)

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import openai
 openai.api_key = "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"
 
 gpt_response = openai.ChatCompletion.create(
-            model="ft:gpt-4o-mini-2024-07-18:personal:question-answering:AakB7wrI",
+            model="ft:gpt-4o-mini-2024-07-18:personal:question-answer-v3:AaodkYTJ:ckpt-step-382",
             messages=[
                 {"role": "system", "content": "You are a Gold Assistant that provides accurate answers to questions about gold."},
                 {"role": "user", "content": "How do you determine how much you should put into gold versus how much you should put to work?"}
